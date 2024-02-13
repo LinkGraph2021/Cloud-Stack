b6a75d2dddc596653b5d8aa0466f592f0cab8370
LinkGraph2021 <projects@linkgraph.io>
2024-02-13 15:38:11 -0600

    fix text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function header() {
                     <div className="w-1/2	flex justify-end items-center gap-8">
                         <Link href="/projects">Reports</Link>
                         <Link href="/projects/new" className='rounded-md bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Create a New Project</Link>
-                        <Link href="/">Sing Up</Link>
+                        <Link href="/">Sign Up</Link>
                         <Link href="/">User Logged In</Link>
                     </div>
                 </div>
