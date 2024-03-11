47a43b571d6c09ec1a9c8be5a12fadab214f3eca
LinkGraph2021 <projects@linkgraph.io>
2024-03-11 13:31:00 -0600

    removed extra btn

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -70,13 +70,6 @@ export default function NewProject() {
     return (
         <div className='flex flex-col gap-28 pb-44'>
             <form className='w-full flex flex-col justify-center' action={formAction}>
-                <button
-                    disabled={isLoading}
-                    type="submit"
-                    className="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all"
-                >
-                    {isLoading ? 'Saving and Exporting...' : 'Save & Export'}
-                </button>
                 <p aria-live="polite" className="">
                     {state?.message}
                 </p>
