fb7a5f21a356b2f413c3ae89b6dc2b5fbb077b84
Felipe Miss <felipe@linkgraph.io>
2024-03-11 10:22:40 -0600

    update code

--- src/components/FormFields/InputF.tsx
+++ src/components/FormFields/InputF.tsx
@@ -17,17 +17,18 @@ export default function InputF( {fieldElement}:InputObj ) {
 
     return (
         <div key={fieldElement.textI} className={`${fieldElement.formInline ? 'flex items-center gap-4':''}`}>
-            <label htmlFor={fieldElement.textI.replaceAll(' ', '-')} className={`${fieldElement.formInline ? 'min-w-36':''} block font-medium leading-6 text-gray-900`}>
+            <label htmlFor={fieldElement.textI.replaceAll(' ', '-').toLowerCase()} className={`${fieldElement.formInline ? 'min-w-36':''} block font-medium leading-6 text-gray-900`}>
                 {fieldElement.textI}
             </label>
             <div className="mt-1">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                     <input
                         type={fieldElement.inputT}
-                        name={fieldElement.textI.replaceAll(' ', '-')}
-                        id={fieldElement.textI.replaceAll(' ', '-')}
+                        name={fieldElement.textI.replaceAll(' ', '-').toLowerCase()}
+                        id={fieldElement.textI.replaceAll(' ', '-').toLowerCase()}
                         autoComplete={fieldElement.inputT}
                         className="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:sm:leading-6"
+                        defaultValue={fieldElement.placeH ? fieldElement.placeH : fieldElement.textI}
                         placeholder={fieldElement.placeH ? fieldElement.placeH : fieldElement.textI}
                     />
                 </div>
