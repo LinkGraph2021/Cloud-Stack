487d6d1bc10c9543de3fc07a201aed299fad630e
Felipe Miss <felipe@linkgraph.io>
2024-03-12 17:49:29 -0600

    update code

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -4,11 +4,13 @@ import path from 'path'
 
 export async function GET(req: Request) {
   var cpath = 'https://cloud-stack-delta.vercel.app';
+  var currentURL = req.headers.get('filename') ? req.headers.get('filename') : 'test';
   if( process.env.NODE_ENV == 'development' ){
     cpath = process.cwd() + '/public';
+  }else if( process.env.NODE_ENV == 'production' ){
+    cpath = require.resolve(path.join(`${cpath}/static/htmls`, `${currentURL}.html`))
   }
-  var currentURL = req.headers.get('filename') ? req.headers.get('filename') : 'test'
-  const buffer = await readFile(path.join(`${cpath}/static/htmls`, `${currentURL}.html`));
+  const buffer = await readFile(cpath);
   const headers = new Headers();
   headers.append('Content-Disposition', `attachment; filename="${currentURL}.htmtl"`);
   headers.append('Content-Type', 'text/html');
