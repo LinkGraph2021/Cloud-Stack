5ec919e874b999adf87668c13aa0bd98994d88b2
Felipe Miss <felipe@linkgraph.io>
2024-03-12 16:51:00 -0600

    update code

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -3,12 +3,13 @@ import { URLSearchParams } from 'next/dist/compiled/@edge-runtime/primitives/url
 import path from 'path'
 
 export async function GET(req: Request) {
-  var cpath = '@';
+  var cpath = '';
   if( process.env.NODE_ENV == 'development' ){
-    cpath = './public';
+    cpath = process.cwd() + '/public';
   }
+  console.log( process.cwd() );
   var currentURL = req.headers.get('filename') ? req.headers.get('filename') : 'test'
-  const buffer = await readFile(path.join(process.cwd(), `${cpath}/static/htmls`, 'test.html'));
+  const buffer = await readFile(path.join(`${cpath}/static/htmls`, 'test.html'));
   const headers = new Headers();
   headers.append('Content-Disposition', `attachment; filename="${currentURL}.htmtl"`);
   headers.append('Content-Type', 'text/html');
