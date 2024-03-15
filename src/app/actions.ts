0d655c01aece6c67775a5dfa084249e61c357196
Felipe Miss <felipe@linkgraph.io>
2024-03-15 10:02:25 -0600

    update create html function

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,26 +3,27 @@ import { writeFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { htmlLayout } from '@/app/general/htmlLayout'
  
-export async function createHtml(prevState: any, formData: FormData) {
+export async function createHtml(rawFormData:any) {
+  
+
   var cpath = 'https://cloud-stack-delta.vercel.app';
-  var fName = formData.get('site-url-to-link-to');
   if( process.env.NODE_ENV == 'development' ){
     cpath = './public/';
   }
-  var htmlResponse =  await htmlLayout(formData);
+  var htmlResponse =  await htmlLayout(rawFormData);
 
   try {
-    writeFileSync(`${cpath}/static/htmls/${fName}.html`, htmlResponse);
+    writeFileSync(`${cpath}/static/htmls/${rawFormData.name}.html`, htmlResponse);
   } catch (error) {
     console.error(error);
     return {
+      response: '',
       message: "couldn't fetch"
     }
     // Expected output: ReferenceError: nonExistentFunction is not defined
     // (Note: the exact output may be browser-dependent)
   }finally {
     return {
-      fileName: fName,
       response: htmlResponse,
       message :'Done'
     }
