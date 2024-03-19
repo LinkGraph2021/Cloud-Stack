84502411f96cb9ba5452ebc0deeff0e3adaea971
Felipe Miss <felipe@linkgraph.io>
2024-03-19 11:18:51 -0600

    update code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,8 @@
 'use client'
-import { useEffect } from 'react';
-import useAuthState from '@/app/profile';
 import Link from 'next/link';
 import { auth } from '@/app/firebase';
 
 export default function header() {
-
-    const user = useAuthState();
-
-    useEffect(() => {
-        if (user) {
-            // User is logged in
-            console.log('User is logged in:', user);
-        } else {
-            // User is logged out
-            console.log('User is logged out');
-        }
-    }, [user]);
 
     const handleLogout = async () => {
         try {
@@ -37,8 +23,8 @@ export default function header() {
                     <div className="w-1/2	flex justify-end items-center gap-8">
                         <Link href="/projects">Reports</Link>
                         <Link href="/projects/new" className='rounded-md bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Create a New Project</Link>
-                        {user && <button onClick={handleLogout}>Sign Out</button>}
-                        {user && <Link href="/">User Logged In</Link>}
+                        <button onClick={handleLogout}>Sign Out</button>
+                        <Link href="/">User Logged In</Link>
                     </div>
                 </div>
             </div>
