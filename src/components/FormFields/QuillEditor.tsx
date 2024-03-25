f40d943c018781e0d669218de25e35b73e565924
Felipe Miss <felipe@linkgraph.io>
2024-03-25 11:47:06 -0600

    update quill file

--- src/components/FormFields/QuillEditor.tsx
+++ src/components/FormFields/QuillEditor.tsx
@@ -1,36 +1,41 @@
-'use client'
 // components/QuillEditor.js
 import React, { useEffect, useRef } from 'react';
-import dynamic from 'next/dynamic';
-//import Quill from 'quill';
-
-const Quill = dynamic(() => import('quill'), { ssr: false }); // Ensure Quill is only loaded on the client side
+import { Quill } from 'quill';
 
 const QuillEditor = ({ defaultValue, onChange, idM, placeH }: any) => {
     const editorRef = useRef<Quill | null>(null); // Ref for Quill instance
     const containerRef = useRef<HTMLDivElement>(null); // Ref for container element
 
     useEffect(() => {
-        if (containerRef.current && !editorRef.current) {
-            const options = {
-                placeholder: placeH,
-                theme: 'snow'
-            };
-            editorRef.current = new Quill(containerRef.current, options);
+        let Quill; // Declare Quill variable locally
 
-            editorRef.current.on('text-change', () => {
-                onChange && onChange(editorRef.current?.root.innerHTML); // Use optional chaining
-            });
+        // Dynamically import Quill when component mounts on the client-side
+        import('quill').then(module => {
+            Quill = module.default || module;
 
-            if (placeH) {
-                editorRef.current.root.innerHTML = placeH;
+            if (containerRef.current && !editorRef.current) {
+                const options = {
+                    placeholder: placeH,
+                    theme: 'snow'
+                };
+                editorRef.current = new Quill(containerRef.current, options);
+
+                editorRef.current?.on('text-change', () => {
+                    onChange && onChange(editorRef.current?.root.innerHTML); // Use optional chaining
+                });
+
+                if (placeH) {
+                    editorRef.current.root.innerHTML = placeH;
+                }
             }
-        }
+        }).catch(error => {
+            console.error('Failed to load Quill', error);
+        });
     }, []);
 
     return (
         <div>
-            <div ref={containerRef} id={`${idM}`}></div>
+            <div ref={containerRef} id={idM}></div>
         </div>
     );
 };
